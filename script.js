b7f9ec5d342765302b367709d9013ff87b7e01ce
Manuel Maisano <manumaisano@gmail.com>
2024-04-09 09:39:17 +0200

    script

---
+++ script.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue;
+
+
+
+//option object.
+createApp({
+  data() { //metodo data()
+    return {
+      apiLink: 'https://flynn.boolean.careers/exercises/api/random/mail',
+
+
+    }
+  },
+  methods: {
+  
+
+  },
+  mounted() {
+    this.getRandomNumber()
+
+  },
+
+}).mount('#app')
